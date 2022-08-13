fd26850a3d0450656fb281676c31b631a24a2d63
Rawnak Chourasia <rnkchr0@gmail.com>
2022-08-14 01:00:25 +0530

    middlewares begin

--- nodews/contact_list/index.js
+++ nodews/contact_list/index.js
@@ -7,6 +7,22 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded());
+
+// middleware 1
+app.use(function (req, res, next) {
+  req.myName = "raw";
+  // console.log("midd 1");
+  next();
+});
+
+// middleware 2
+app.use(function (req, res, next) {
+  console.log(req.myName);
+  // console.log("midd 2");
+  next();
+});
+
 var contact_list = [
   {
     name: "Rawnak",
@@ -38,10 +54,22 @@ app.get("/practice", function (req, res) {
 });
 
 app.get("/contacts", function (req, res) {
+  console.log(req.myName);
   return res.render("contacts", {
     title: "contacts",
     contactList: contact_list,
   });
+});
+
+app.post("/create-contact", function (req, res) {
+  // console.log(req.body);
+  // contact_list.push({
+  //   name: req.body.name,
+  //   phone: req.body.phone,
+  // });
+  contact_list.push(req.body);
+  // return res.redirect("/contacts");
+  return res.redirect("back");
 });
 
 app.listen(port, function (err) {
