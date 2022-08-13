5b568b9dc0416093af1825adddffc6bfd553a6b8
Rawnak Chourasia <rnkchr0@gmail.com>
2022-08-13 19:01:43 +0530

    contacts file

--- nodews/contact_list/index.js
+++ nodews/contact_list/index.js
@@ -7,6 +7,21 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+var contact_list = [
+  {
+    name: "Rawnak",
+    phone: 92834902,
+  },
+  {
+    name: "loki",
+    phone: 349302934,
+  },
+  {
+    name: "junior",
+    phone: 8346355433,
+  },
+];
+
 app.get("/", function (req, res) {
   //   console.log(req);
   //   console.log(__dirname); // Prints current inde.js directory name
@@ -19,6 +34,13 @@ app.get("/", function (req, res) {
 app.get("/practice", function (req, res) {
   return res.render("practice", {
     title: "it's practice",
+  });
+});
+
+app.get("/contacts", function (req, res) {
+  return res.render("contacts", {
+    title: "contacts",
+    contactList: contact_list,
   });
 });
 
